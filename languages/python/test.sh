ba6ec9fe90657ba2db0b777284d816efc57db646
J. King <jking@jkingweb.ca>
2026-05-09 14:18:46 -0400

    Adapt Python

--- languages/python/test.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-function test_one {
-    uv run --locked --no-sync --no-cache "$2/test-one.py" "$1"
-}
--- languages/python/version.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-uv export -q --no-cache | grep ^mf2py== | cut -d ' ' -f 1 | cut -d '=' -f 3
